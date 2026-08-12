4618258e0fdb3a6bbc14a9e542b8d80e46bf619f
LYT259 <liuyutong259@iloud.com>
2026-08-12 11:13:33 +0800

    fix(web_ui): 恢复控制参数滑块为浏览器原生样式
    上一版（#43）添加的自定义 thumb 样式导致拖拽点变成正圆形、
    垂直偏上、且 shadow 环仍透出轨道。移除全部自定义 thumb 样式，
    恢复 accent-cyan-500 浏览器原生渲染：椭圆形、垂直居中、不透明。

--- web_ui/frontend/src/components/drive/ParameterPanel.tsx
+++ web_ui/frontend/src/components/drive/ParameterPanel.tsx
@@ -26,13 +26,7 @@ const ParamSlider: React.FC<ParamSliderProps> = ({ label, value, min, max, step,
       step={step}
       value={value}
       onChange={(e) => onChange(Number(e.target.value))}
-      className="w-full h-1.5 bg-zinc-800 rounded-lg appearance-none cursor-pointer
-        [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-3.5 [&::-webkit-slider-thumb]:h-3.5
-        [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white [&::-webkit-slider-thumb]:cursor-pointer
-        [&::-webkit-slider-thumb]:-mt-1 [&::-webkit-slider-thumb]:shadow-[0_0_0_2px_#09090b]
-        [&::-moz-range-thumb]:w-3.5 [&::-moz-range-thumb]:h-3.5 [&::-moz-range-thumb]:rounded-full
-        [&::-moz-range-thumb]:bg-white [&::-moz-range-thumb]:border-none [&::-moz-range-thumb]:cursor-pointer
-        [&::-moz-range-thumb]:shadow-[0_0_0_2px_#09090b]"
+      className="w-full h-1.5 bg-zinc-800 rounded-lg appearance-none cursor-pointer accent-cyan-500"
     />
   </div>
 );
